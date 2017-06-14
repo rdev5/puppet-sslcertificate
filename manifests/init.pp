1cfe3909f97cb52aaf22741ec22922b874557bfd
Matt Borja <matt.borja@gmail.com>
2017-06-14 09:00:15 -0700

    Cleanup import and inspect scripts after install

--- manifests/init.pp
+++ manifests/init.pp
@@ -121,4 +121,14 @@ define sslcertificate (
     logoutput => true,
     require   => [File["inspect-${name}-certificate.ps1"], File["import-${name}-certificate.ps1"]],
   }
+  
+  exec { "Cleanup ${scripts_dir}\\import-${name}.ps1":
+    provider => 'powershell',
+    command => "Remove-Item -Force ${scripts_dir}\\import-${name}.ps1",
+    require => Exec["Install-${name}-SSLCert"],
+  } ->
+  exec { "Cleanup ${scripts_dir}\\inspect-${name}.ps1":
+    provider => 'powershell',
+    command => "Remove-Item -Force ${scripts_dir}\\inspect-${name}.ps1",
+  }
 }
